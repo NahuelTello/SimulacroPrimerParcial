932c108a1900b598c7b9bd01eb0391388de72e2b
NahuelTello <matias.tello@est.fi.uncoma.edu.ar>
2022-04-12 20:34:14 -0300

    Changes Cuota

--- Cuota.php
+++ Cuota.php
@@ -1,4 +1,59 @@
 <?php
 class Cuota{
-    
+    private $numero;
+    private $monto_cuota;
+    private $monto_interes;
+    private $cancelada; //Boolean 
+
+    public function __construct($ID, $montoCuota, $montoInteres, $cuotaCancelada)
+    {
+        $this->numero = $ID;
+        $this->monto_cuota = $montoCuota;
+        $this->monto_interes = $montoInteres;
+        $this->cancelada = $cuotaCancelada;
+    }
+
+    public function getNumero(){
+        return $this->numero;
+    }
+
+    public function setNumero($nuevoID){
+        $this->numero = $nuevoID;
+    }
+
+    public function get()
+    {
+    }
+
+    public function set()
+    {
+    }
+
+    public function get()
+    {
+    }
+
+    public function set()
+    {
+    }
+
+    public function get()
+    {
+    }
+
+    public function set()
+    {
+    }
+
+
+
+
+
+
+
+
+    public function __toString()
+    {
+        
+    }
 }
